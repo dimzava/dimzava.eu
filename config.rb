6ef22b9ed1f084008a8eec5521dcd3e4cbe1306f
dimzava <dimzava@gmail.com>
2015-01-10 01:11:00 +0200

    Add middleman sources.

---
+++ config.rb
@@ -0,0 +1,104 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+helpers do
+    def nav_link_to(link, url, opts={})
+      if active(url)
+        prefix = '<li role="presentation" class="active">'
+      else
+        prefix = '<li role="presentation">'
+      end
+      prefix + link_to(link, url, opts) + "</li>"
+    end
+    
+    def active(url)
+      result = false
+      if (url == '/' && current_resource.url == url) || ( url != '/' && (current_resource.url.include? url) )
+        result = true
+      end
+      result
+    end
+end
+
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css, inline: true
+
+  # Minify Javascript on build
+  activate :minify_javascript, inline: true
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+  
+  # Enable uniquely named assets for cacheability
+  activate :asset_hash
+
+  # Added by dimzava on 10/1/2015
+  activate :minify_html
+end
+
+activate :directory_indexes
